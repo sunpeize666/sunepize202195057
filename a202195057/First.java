696cf94273b4405800bc195dce993fd385a7a176
sunpeize666 <144121626+sunpeize666@users.noreply.github.com>
2024-03-08 13:09:04 +0900

    Add files via upload

---
+++ a202195057/First.java
@@ -0,0 +1,13 @@
+package a202195057;
+/*
+ * 2024. 3.08
+ * 컴퓨터공학부 202195057
+ */
+public class First {
+  public static void main(String[] args) {
+	  System.out.println("안녕하세요");
+	  System.out.println("손패택입니다");
+	  System.out.println("반갑습니다."+"첫 번째 자바 프로램입니다.");
+	  
+	  }
+}
